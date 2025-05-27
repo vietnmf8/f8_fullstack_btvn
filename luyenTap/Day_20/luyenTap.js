6052901281334f14309f628d832f4bd1adf5765e
viet <vietnmf8@fullstack.edu.vn>
2025-05-27 18:58:04 +0700

    LuyenTap

---
+++ luyenTap/Day_20/luyenTap.js
@@ -0,0 +1,21 @@
+/* Su dung "Create Element" trong DOM */
+
+/* Buoc 1: Tao mang du lieu */
+// Mang Header - Tieu de tung cot cua table
+const headers = [
+    {name: 'id', text: 'Id'},
+    {name: 'name', text: 'Ten'},
+    {name: 'address', text: 'Dia chi'},
+    {name: 'age', text: 'Tuoi'}
+]
+
+// Mang danh sach nhan vien
+const employees = [
+    {id: 1, name: 'Pham Cong Tin', address: 'Hoai Duc - Ha Noi', age: 27},
+    {id: 2, name: 'Nguyen Nam Tao', address: 'Hoai Duc - Ha Noi', age: 27},
+    {id: 1, name: 'Pham Xuan Bac', address: 'Hoai Duc - Ha Noi', age: 27},
+]
+
+/* Buoc 2: Tao table */
+
+/* Thead */
